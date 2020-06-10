bce7a43231b5a2d0f071073cb25a36289fdae59b
Yoni Touboul <yonitouboul@hotmail.com>
2020-06-10 15:12:39 +0200

    Add competences to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_103017) do
+ActiveRecord::Schema.define(version: 2020_06_10_131153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_103017) do
     t.string "address"
     t.text "bio"
     t.string "website"
+    t.string "competences"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
