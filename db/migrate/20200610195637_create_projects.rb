9982cd4e0c663f2cd78d8a6d444d4736033f158c
Yoni Touboul <yonitouboul@hotmail.com>
2020-06-10 22:17:23 +0200

    Project migration

--- db/migrate/20200610195637_create_projects.rb
+++ db/migrate/20200610195637_create_projects.rb
@@ -1,12 +1,12 @@
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
-      t.title :string
-      t.description :text
-      t.categories :string, array: true, :default => []
-      t.address :string
-      t.open_to_collab :boolean, default: true
-      t.finished :boolean, default: false
+      t.string :title
+      t.text :description
+      t.string :categories, array: true, :default => []
+      t.string :address
+      t.boolean :open_to_collab, default: true
+      t.boolean :finished, default: false
       t.references :users
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_153418) do
+ActiveRecord::Schema.define(version: 2020_06_10_195637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "categories", default: [], array: true
+    t.string "address"
+    t.boolean "open_to_collab", default: true
+    t.boolean "finished", default: false
+    t.bigint "users_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["users_id"], name: "index_projects_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
