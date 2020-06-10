6f0cacbbef76de188eca7087ce4f7055d81732e4
Yoni Touboul <yonitouboul@hotmail.com>
2020-06-11 00:54:45 +0200

    New, create, show controller done + simple views

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
 
   CATEGORIES_LIST = ["Musique", "Street Art", "Architecture", "Arts Vivants / Arts de la scène", "Arts Numériques",
   "Littérature", "Cinéma", "Arts Visuels", "Scuplture", "Arts Médiatiques", "Bandes Dessinées", "Jeux Vidéo & Multimédia" ]
-  validates :title, :description, :categories, :address, :open_to_collab, presence: true
+  validates :title, :description, :categories, :address, presence: true
   belongs_to :user
 
   before_save :clean_blank_categories
