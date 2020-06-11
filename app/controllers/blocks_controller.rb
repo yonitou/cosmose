253f26ae1133bbd1f7d54d75a215b42a9f1cbc20
Thibaud Gerin <thibaud.gerin@gmail.com>
2020-06-11 18:00:39 +0200

    added automatic youtube embed when pasting a youtube url

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -3,6 +3,7 @@ class BlocksController < ApplicationController
   def create
     @block = Block.new(block_params)
     @block.user = current_user
+    block_content_type
     if params[:project_id]
       @block.project_id = params[:project_id]
       @block.user = current_user
@@ -19,6 +20,17 @@ class BlocksController < ApplicationController
 
   def block_params
     params.require(:block).permit(:content, :url, :private, :project_id, :photo)
+  end
+
+  def block_content_type
+    if @block.content.include?("youtu")
+        regex = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+        match = regex.match(@block.content)
+        if match && !match[1].blank?
+          id = match[1]
+        end
+      @block.content = "<lite-youtube videoid=#{id}></lite-youtube>"
+    end
   end
 
 end
