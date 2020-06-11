a8d1edc5bb11fdcdc46a15cb232b813ad8d783a1
Thibaud Gerin <thibaud.gerin@gmail.com>
2020-06-11 19:01:58 +0200

    blocks img & video working

--- app/models/block.rb
+++ app/models/block.rb
@@ -4,5 +4,5 @@ class Block < ApplicationRecord
   has_one_attached :audio
   validates_presence_of :content
   validates_attachment_content_type :photo, content_type: ['image/jpg', 'image/jpeg', 'image/png']
-  validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
+  validates_attachment_content_type :audio, content_type: [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
 end
