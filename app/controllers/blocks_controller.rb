edc623744c748fa3caa1dd4f84d4f42f54c33b58
Thibaud Gerin <thibaud.gerin@gmail.com>
2020-06-12 16:04:27 +0200

    block audio working on local

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,10 +1,10 @@
 class BlocksController < ApplicationController
-  before_action :skip_pundit?
   
   def create
     @block = Block.new(block_params)
+    authorize(@block)
     @block.user = current_user
-    check_upload_type
+    check_if_yt
     if params[:project_id]
       @block.project_id = params[:project_id]
       @block.user = current_user
@@ -23,20 +23,13 @@ class BlocksController < ApplicationController
     params.require(:block).permit(:content, :url, :private, :project_id, :upload)
   end
 
-  def check_upload_type
-    if @block.upload.attached?
-      if @block.upload.audio?
-        @block.content = "<%= audio_tag url_for(block.upload), controls: true, preload: 'none' %>".html_safe
-      elsif @block.upload.image?
-        @block.content = "<%= image_tag(block.photo, width: '150px') %>".html_safe
-      end
-    end
+  def check_if_yt
     if @block.content.include?("youtu")
-        regex = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
-        match = regex.match(@block.content)
-        if match && !match[1].blank?
-          id = match[1]
-        end
+      regex = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+      match = regex.match(@block.content)
+      if match && !match[1].blank?
+        id = match[1]
+      end
       @block.content = "<lite-youtube videoid=#{id}></lite-youtube>"
     end
   end
