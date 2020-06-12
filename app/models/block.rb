5edb771899372f915717d3fe62c700f6bf353b9a
Thibaud Gerin <thibaud.gerin@gmail.com>
2020-06-12 14:35:24 +0200

    replaced block audio & photo by upload

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,7 +1,6 @@
 class Block < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo
-  has_one_attached :audio
+  has_one_attached :upload
   validates_presence_of :content
   # validates_attachment_content_type :photo, content_type: ['image/jpg', 'image/jpeg', 'image/png']
   # validates_attachment_content_type :audio, content_type: [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
