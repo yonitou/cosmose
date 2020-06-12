f5cb98452d6ca73430d891c36078a7630dac6b00
Thibaud Gerin <thibaud.gerin@gmail.com>
2020-06-12 14:55:37 +0200

    save

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,9 +1,10 @@
 class BlocksController < ApplicationController
+  before_action :skip_pundit?
   
   def create
     @block = Block.new(block_params)
     @block.user = current_user
-    check_if_yt
+    check_upload_type
     if params[:project_id]
       @block.project_id = params[:project_id]
       @block.user = current_user
@@ -19,10 +20,17 @@ class BlocksController < ApplicationController
   private
 
   def block_params
-    params.require(:block).permit(:content, :url, :private, :project_id, :photo, :audio)
+    params.require(:block).permit(:content, :url, :private, :project_id, :upload)
   end
 
-  def check_if_yt
+  def check_upload_type
+    if @block.upload.attached?
+      if @block.upload.audio?
+        @block.content = "<%= audio_tag url_for(block.upload), controls: true, preload: 'none' %>".html_safe
+      elsif @block.upload.image?
+        @block.content = "<%= image_tag(block.photo, width: '150px') %>".html_safe
+      end
+    end
     if @block.content.include?("youtu")
         regex = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
         match = regex.match(@block.content)
@@ -32,5 +40,4 @@ class BlocksController < ApplicationController
       @block.content = "<lite-youtube videoid=#{id}></lite-youtube>"
     end
   end
-
 end
