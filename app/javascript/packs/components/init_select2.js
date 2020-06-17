b38648c95d1cfea98a05524c18543d5b830ae7b0
Yoni Touboul <yonitouboul@hotmail.com>
2020-06-17 10:14:47 +0200

    New project form (categories on block), button open to collab, padding on main container

--- app/javascript/packs/components/init_select2.js
+++ app/javascript/packs/components/init_select2.js
@@ -1,6 +1,15 @@
 const initSelect2 = () => {
-  if (document.querySelector('.required-inputs-project-1')) {
+  if ($('.required-inputs-project-1')) {
     $('.select2').select2({ width: '100%', multiple: true, maximumSelectionLength: 3});
+    $(".project-categories").on("click", function () {
+    let oldValue = $('.select2').val();
+      if (oldValue.length < 3) {
+        oldValue.push($(this).text());
+        $('.select2').val(oldValue).trigger('change');
+      } else {
+        alert('Vous ne pouvez pas sélectionner plus de 3 catégories par projet')
+      }
+    });
   } else {
     $('.select2').select2({ width: '100%', multiple: true});
   }
