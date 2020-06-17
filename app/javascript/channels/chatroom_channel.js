62baff9dfdc2cd90488790e3cdfc6c24ed06c429
Yoni Touboul <yonitouboul@hotmail.com>
2020-06-17 14:30:43 +0200

    Action cable working with correct broadcast

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,7 +1,7 @@
 import consumer from "./consumer";
 
 const initChatroomCable = () => {
-  const messagesContainers = document.getElementsByClassName('messages-box');
+  const messagesContainers = document.getElementsByClassName('messages');
   if (messagesContainers.length > 0) {
     Array.from(messagesContainers).forEach((messagesContainer) => {
       const id = messagesContainer.dataset.chatroomId;
