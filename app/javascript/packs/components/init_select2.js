3bc292c455931c9667c28cf3edeeb04839fe01ea
Yoni Touboul <yonitouboul@hotmail.com>
2020-06-18 13:21:33 +0200

    Select 2 hide on new project + buttons working perfectly

--- app/javascript/packs/components/init_select2.js
+++ app/javascript/packs/components/init_select2.js
@@ -1,13 +1,26 @@
 const initSelect2 = () => {
   if ($('.required-inputs-project-1')) {
     $('.select2').select2({ width: '100%', multiple: true, maximumSelectionLength: 3});
+    $('.select2').hide();
     $(".project-categories").on("click", function () {
-    let oldValue = $('.select2').val();
-      if (oldValue.length < 3) {
-        oldValue.push($(this).text());
+      let oldValue = $('.select2').val();
+      if (oldValue.includes($(this).text())) {
+        const i = oldValue.indexOf($(this).text());
+        if (i > -1) {
+          oldValue.splice(i, 1);
+        }
         $('.select2').val(oldValue).trigger('change');
+        $(this).removeClass('active');
       } else {
-        alert('Vous ne pouvez pas sélectionner plus de 3 catégories par projet')
+
+        if ($('.select2').val().length >= 3) {
+          alert('Vous ne pouvez pas sélectionner plus de 3 catégories par projet');
+
+        } else {
+          oldValue.push($(this).text());
+          $('.select2').val(oldValue).trigger('change');
+          $(this).addClass('active');
+        }
       }
     });
   } else {
