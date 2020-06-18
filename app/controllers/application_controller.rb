508cb5719a8c246564b60470d70d7571e096b94f
Yoni Touboul <yonitouboul@hotmail.com>
2020-06-18 17:53:33 +0200

    Meta tags, favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,8 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(^notifications$)|(^users$)|(^messages$)/
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
