a3fe870b8893b2cb3f742f2f889a89cbd5f12166
yonitou <61981305+yonitou@users.noreply.github.com>
2020-06-18 16:25:58 +0200

    Update chatroom_form.js

--- app/javascript/packs/components/chatroom_form.js
+++ app/javascript/packs/components/chatroom_form.js
@@ -1,7 +1,6 @@
 const initChatroomForm = () => {
   if (document.querySelector('#new_message')) {
       const message = document.querySelector('.active_footer input');
-      console.log('hello');
       message.focus();
   };
 };
